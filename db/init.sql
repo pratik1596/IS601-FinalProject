133a40cb455399ade2a0f3f5c769826014a523e3
pratik1596 <83378087+pratik1596@users.noreply.github.com>
2021-08-02 22:53:22 -0700

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE teamsData;
 use teamsData;
 
 CREATE TABLE IF NOT EXISTS mlb_teams (
-    'id' INT AUTO INCREMENT,
+    'id' INT AUTO_INCREMENT,
     `Team` VARCHAR(12) CHARACTER SET utf8,
     `Payroll_millions` NUMERIC(5, 2),
     `Wins` INT,
